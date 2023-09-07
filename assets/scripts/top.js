258299a8d0a60db74dcc74c86efc123e33735521
MecosLB <64880524+MecosLB@users.noreply.github.com>
2023-09-07 11:48:54 -0600

    Add files via upload

--- assets/scripts/top.js
+++ assets/scripts/top.js
@@ -5,16 +5,28 @@
         toFocus = document.querySelector('.to-focus'),
         halfViewer = unFocus.offsetWidth / 2;
 
-    const sections = [...document.querySelectorAll("#top .slide")],
+    const activeTouches = [];
+
+    const sections = [...document.querySelectorAll('#top .slide')],
         observer = new IntersectionObserver(callback, options);
 
     const getCoordinates = (e) => {
-        var rect = e.target.getBoundingClientRect();
-        var x = e.clientX - rect.left - halfViewer;
-        var y = e.clientY - rect.top - halfViewer;
+        let rect = e.target.getBoundingClientRect(),
+            x = e.clientX - rect.left - halfViewer,
+            y = e.clientY - rect.top - halfViewer;
 
-        unFocus.style.transform = "translate(" + x + "px," + y + "px)";
-        unFocus.style.backgroundPosition = -x + "px" + " " + -y + "px";
+        unFocus.style.transform = 'translate(' + x + 'px,' + y + 'px)';
+        unFocus.style.backgroundPosition = -x + 'px' + ' ' + -y + 'px';
+    };
+
+    const getCoordinatesMobile = (e, xR = 0, yR = 0) => {
+        let rect = e.target.getBoundingClientRect(),
+            x = xR - rect.left - halfViewer,
+            y = yR - rect.top - halfViewer;
+
+        unFocus.style.opacity = '1';
+        unFocus.style.transform = 'translate(' + x + 'px,' + y + 'px)';
+        unFocus.style.backgroundPosition = -x + 'px' + ' ' + -y + 'px';
     };
 
     document.addEventListener('DOMContentLoaded', () => {
@@ -24,7 +36,7 @@
 
             // ADD DELAY TO EACH CHILD
             sectionChildren.forEach((el, index) => {
-                el.style.setProperty("--delay", `${index * 250}ms`);
+                el.style.setProperty('--delay', `${index * 250}ms`);
             });
 
             observer.observe(section);
@@ -32,8 +44,29 @@
 
         // UNFOCUS EFFECT
         toFocus.addEventListener('mousemove', getCoordinates);
-        toFocus.addEventListener('touchstart', getCoordinates);
-        toFocus.addEventListener('touchmove', getCoordinates);
-        toFocus.addEventListener('touchend', getCoordinates);
+
+        // TOUCH IMIPLEMENTATION
+        toFocus.addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            const touches = e.changedTouches;
+
+            for (let touch of touches)
+                getCoordinatesMobile(e, touch.clientX, touch.clientY);
+        });
+
+        toFocus.addEventListener('touchend', (e) => {
+            e.preventDefault();
+            const touches = e.changedTouches;
+
+            unFocus.style.opacity = '0';
+        });
+
+        toFocus.addEventListener('touchmove', (e) => {
+            e.preventDefault();
+            const touches = e.changedTouches;
+
+            for (let touch of touches)
+                getCoordinatesMobile(e, touch.clientX, touch.clientY);
+        });
     });
 })();
